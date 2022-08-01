e14c072b3d72ccdb32caa062dc9d14a8c5610ccb
MaratMuhametshin <fat_fly@mail.ru>
2022-08-01 15:54:35 +0500

    calc 0.0

---
+++ src/Calc.java
@@ -0,0 +1,9 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+public class Calc {
+    public static void main(String[] args) {
+        out.print("hi");
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
